3e8860828a651493cf303e8686294c446cb8bf15
mo <mo.kadib.alban@hva.nl>
2024-03-05 15:29:54 +0100

    used the .env

--- database/db-connect.js
+++ database/db-connect.js
@@ -1,11 +1,13 @@
 const {Sequelize} = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const db = new Sequelize({
     dialect: 'postgres',
-    database: 'concept',
-    username: 'root',
-    password: 'root',
-    host: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
     port: 5432
 });
 
